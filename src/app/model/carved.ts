2e86562fa4f790c4f9fd4d947541ff76d62490c9
Ossqui <oscarsomozaperdigon@gmail.com>
2019-05-25 20:14:44 +0200

    Creado el modelo de talla de imagen, creado el servicio y añadida la primera parte de este

---
+++ src/app/model/carved.ts
@@ -0,0 +1,14 @@
+export interface Carved {
+
+    id?: string;
+    name?: string;
+    author?: string;
+    ageOfCarved?: string;
+    style?: string;
+    note?: string;
+    material?: string;
+    description?: string;
+    image?: string;
+    brotherhood?: string;
+
+}
---
+++ src/app/services/carved.service.ts
@@ -0,0 +1,37 @@
+import { Carved } from './../model/carved';
+import { Injectable } from '@angular/core';
+import { isNullOrUndefined } from 'util';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarvedService {
+  private carved: Carved = {}
+  private name: string;
+  private author: string;
+  private ageOfCarved: string;
+  private style: string;
+  private note: string;
+  private material: string;
+  private description: string;
+  private image: string;
+  private brotherhood: string;
+
+  constructor() { }
+
+  part1(name: string, author: string, ageOfCarved: string, style: string) {
+    var count = 0;
+    if (name.length <= 60 && name.length > 0 && !isNullOrUndefined(name)) { this.name = name; count = count + 1; };
+    if (author.length <= 40 && author.length > 0 && !isNullOrUndefined(author)) { this.author = author; count = count + 1; };
+    if (ageOfCarved.length <= 20 && ageOfCarved.length > 0 && !isNullOrUndefined(ageOfCarved)) { this.ageOfCarved = ageOfCarved; count = count + 1; };
+    if (style.length <= 50 && style.length > 0 && !isNullOrUndefined(style)) { this.style = style; count = count + 1; };
+    return new Promise ((resolve, rejected) =>{
+      if(count == 4){
+        return resolve("OK");
+      }else{
+        return rejected("ERROR");
+      }
+    })
+    
+  }
+}
