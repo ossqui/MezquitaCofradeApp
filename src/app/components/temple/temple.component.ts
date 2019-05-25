fb3dfbbc6e9041e6d40ecdf6aa5f3ec39701deba
Ossqui <oscarsomozaperdigon@gmail.com>
2019-05-25 17:21:16 +0200

    Creada la galería y el servicio para añadir imagenes. Cada templo carga sus  propias imagenes

--- src/app/components/temple/temple.component.ts
+++ src/app/components/temple/temple.component.ts
@@ -1,6 +1,10 @@
+import { DataService } from './../../services/data.service';
+import { AuthService } from './../../services/auth.service';
 import { ModalController, NavParams } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { Temple } from 'src/app/model/temple';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { imageGallery } from 'src/app/model/imageGallery';
 
 @Component({
   selector: 'app-temple',
@@ -8,18 +12,69 @@ import { Temple } from 'src/app/model/temple';
   styleUrls: ['./temple.component.scss'],
 })
 export class TempleComponent implements OnInit {
+  listImages: imageGallery[] = [];
   temple: Temple;
 
   constructor(
+    private camera: Camera,
     private ModalController: ModalController,
-    private NavParams: NavParams
+    private NavParams: NavParams,
+    private AuthService: AuthService,
+    private DataService: DataService
   ) { }
 
   ngOnInit() {
     this.temple = this.NavParams.get('temple');
+    this.DataService.getImagesTemple(this.temple.id).then(imagesGallery =>{
+     this.listImages = imagesGallery;
+    })
   }
 
-  closeModal(){
+  closeModal() {
     this.ModalController.dismiss();
+  }
+
+  galleryisNull(){
+    if(this.listImages.length>0){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  photographic() {
+    var image:imageGallery;
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true,
+      targetWidth: 200,
+      saveToPhotoAlbum: false
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      image = {
+        idFather: this.temple.id,
+        code: 'data:image/jpeg;base64,' + imageData
+      }
+      this.DataService.addImageGallery(image);
+      this.DataService.getImagesTemple(this.temple.id).then(imagesGallery =>{
+        this.listImages = imagesGallery;
+       })
+
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+
+  permisions() {
+    if ((this.AuthService.returnPermisions() == "2") || (this.AuthService.returnPermisions() == "3")) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
